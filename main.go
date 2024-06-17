ec420d21eac23a1f7be4955fe3fc5050d060722a
mloughton <matt.loughton@gmail.com>
2024-06-17 14:50:30 +0100

    C: improved routing and implemented html template GET

--- main.go
+++ main.go
@@ -4,22 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
 
 type apiConfig struct {
-	filserverHits int
+	fileserverHits int
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.filserverHits++
-		next.ServeHTTP(w, r)
-	})
-}
-
-func middlewareLog(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", r.Method, r.URL.Path)
+		cfg.fileserverHits++
 		next.ServeHTTP(w, r)
 	})
 }
@@ -27,16 +21,33 @@ func middlewareLog(next http.Handler) http.Handler {
 func (cfg *apiConfig) HandleGetMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.filserverHits)))
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
 }
 
 func (cfg *apiConfig) HandleResetMetrics(w http.ResponseWriter, r *http.Request) {
-	cfg.filserverHits = 0
+	cfg.fileserverHits = 0
 	w.WriteHeader(http.StatusOK)
 }
 
 func appHandler() http.Handler {
 	return http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+}
+
+func (cfg *apiConfig) HandleGetAdminMetrics(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("./admin/metrics/index.html")
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	err = tmpl.Execute(w, cfg.fileserverHits)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
 }
 
 func main() {
@@ -45,14 +56,16 @@ func main() {
 
 	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(appHandler()))
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
 
-	mux.HandleFunc("/metrics", apiCfg.HandleGetMetrics)
-	mux.HandleFunc("/reset", apiCfg.HandleResetMetrics)
+	mux.HandleFunc("GET /api/metrics", apiCfg.HandleGetMetrics)
+	mux.HandleFunc("/api/reset", apiCfg.HandleResetMetrics)
+
+	mux.HandleFunc("GET /admin/metrics/", apiCfg.HandleGetAdminMetrics)
 
 	server := &http.Server{
 		Handler: mux,
